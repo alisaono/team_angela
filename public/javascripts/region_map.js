40217b01a61c55028ea07aeac16f5a6902a8893d
Alisa Ono <alisao.mit@gmail.com>
2018-12-07 20:13:07 -0500

    Add selection

--- public/javascripts/region_map.js
+++ public/javascripts/region_map.js
@@ -1,52 +1,58 @@
+let stateNames = {} // abbreviation to name
+let incomeData = []
+let selections = new Set()
+
 google.charts.load('current', {
   packages:['geochart'],
   mapsApiKey: 'AIzaSyAjtJGdq1tPS1DfKqylSSuHii6VvIDGxsU', // TODO: domain restrict API access
   callback: function() {
-    drawStateIncomes('state_incomes_map')
-    drawStatePopulations('state_populations_map')
+    loadIncomes()
   }
 })
 
-function drawStateIncomes(divID) {
+function loadIncomes() {
   $.getJSON("/api/income/states",function(data) {
-    let dataArray = [['State', 'Income']] // data header
     for (let row of data.rows) {
-      dataArray.push([
+      stateNames[`US-${row.abbreviation}`] = row.name
+      incomeData.push([
         // v is the value, f is the content of tooltip.
         {v: `US-${row.abbreviation}`, f: row.name},
         row.income
       ])
     }
-
-    let dataTable = google.visualization.arrayToDataTable(dataArray)
-    let formatter = new google.visualization.NumberFormat({prefix: '$', fractionDigits: 0})
-    formatter.format(dataTable, 1)
-
-    drawRegionMap(dataTable, divID)
+    updateMap()
   })
 }
 
-function drawStatePopulations(divID) {
-  $.getJSON("/api/population/states",function(data) {
-    let dataArray = [['State', 'Population']] // data header
-    for (let row of data.rows) {
-      dataArray.push([
-        // v is the value, f is the content of tooltip.
-        {v: `US-${row.abbreviation}`, f: row.name},
-        // value == content of tooltip for row.population
-        row.population
-      ])
+function updateMap() {
+  let dataArray = [['State', 'Income']] // data header
+  for (let d of incomeData) {
+    if (!selections.has(d[0]['v'])) {
+      dataArray.push(d)
     }
-    let dataTable = google.visualization.arrayToDataTable(dataArray)
-    drawRegionMap(dataTable, divID)
-  })
+  }
+  let dataTable = google.visualization.arrayToDataTable(dataArray)
+  let formatter = new google.visualization.NumberFormat({prefix: '$', fractionDigits: 0})
+  formatter.format(dataTable, 1)
+  drawRegionMap(dataTable, 'state_incomes_map')
 }
 
 function drawRegionMap(dataTable, divID) {
   let options = {
     region: 'US',
     resolution: 'provinces',
+    datalessRegionColor: '#888'
   }
   let chart = new google.visualization.GeoChart(document.getElementById(divID))
   chart.draw(dataTable, options)
+  google.visualization.events.addListener(chart, 'regionClick', function(data) {
+    if (selections.has(data.region)) {
+      selections.delete(data.region)
+      $(`#selected_list > li.${data.region}`).remove()
+    } else {
+      selections.add(data.region)
+      $('#selected_list').append(`<li class="${data.region}"> ${stateNames[data.region]}</li>`)
+    }
+    updateMap()
+  })
 }
