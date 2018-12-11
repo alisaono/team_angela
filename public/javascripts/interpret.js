8c7c8aee95d83798b34b63be049acf7675ff0b78
Yang Yan <yangawesome@gmail.com>
2018-12-11 18:39:08 -0500

    .

---
+++ public/javascripts/interpret.js
@@ -0,0 +1,91 @@
+let ptThresh = 0.0005
+
+//gesture analysis functions return a list [a, b] where a is time taken for query in ms and b is the set of states selected
+function stabbingGesture(gesture) {
+	let result = new Set([])
+	for (let a = 0; a < 50; a++) {
+		for (let b = 0; b < regionPts[a].length; b++) {
+			for (let c = 0; c < gesture.length - 1; c++) {
+				//test if regionPts[a][b] is close enough to line segment gesture[c], gesture[c + 1]
+				let dist = distToSegment(regionPts[a][b], gesture[c], gesture[c + 1])
+				if (dist < ptThresh) {
+					result.add(stateCodes[a])
+				}
+			}
+		}
+	}
+	return [gesture[gesture.length - 1][2], Array.from(result)]
+}
+
+function wrappingInclusiveGesture(gesture) {
+	let result = new Set([])
+	let gesturePts = []
+
+	//compute gesturePts with .x and .y of gesture points to pass to convex hull function
+	for (let a = 0; a < gesture.length; a++) {
+		gesturePts.push({ x: gesture[a][0], y: gesture[a][1] })
+	}
+
+	for (let a = 0; a < 50; a++) {
+		for (let b = 0; b < regionPts[a].length; b++) {
+			if (ptInPoly({ x: regionPts[a][b][0], y: regionPts[a][b][1] }, gesturePts)) {
+				result.add(stateCodes[a])
+				break
+			}
+		}
+	}
+	return [gesture[gesture.length - 1][2], Array.from(result)]
+}
+
+function wrappingExclusiveGesture(gesture) {
+	let result = new Set([])
+	let gesturePts = []
+
+	//compute gesturePts with .x and .y of gesture points to pass to convex hull function
+	for (let a = 0; a < gesture.length; a++) {
+		gesturePts.push({ x: gesture[a][0], y: gesture[a][1] })
+	}
+
+	for (let a = 0; a < 50; a++) {
+		let shouldAdd = true
+		for (let b = 0; b < regionPts[a].length; b++) {
+			if (!ptInPoly({ x: regionPts[a][b][0], y: regionPts[a][b][1] }, gesturePts)) {
+				shouldAdd = false
+				break
+			}
+		}
+		if (shouldAdd) {
+			result.add(stateCodes[a])
+		}
+	}
+	return [gesture[gesture.length - 1][2], Array.from(result)]
+}
+
+function hullInclusiveGesture(gesture) {
+	let result = new Set([])
+	let gesturePts = []
+
+	//compute gesturePts with .x and .y of gesture points to pass to convex hull function
+	for (let a = 0; a < gesture.length; a++) {
+		gesturePts.push({ x: gesture[a][0], y: gesture[a][1] })
+	}
+
+	for (let a = 0; a < 50; a++) {
+		//add state if state convex hull @regionHulls[a] intersects gesture polygon at gesturePts
+		for (let b = 0; b < regionHulls[a].length - 1; b++) {
+			for (let c = 0; c <= 20; c++) {
+				if (ptInPoly({
+					x: regionHulls[a][b].x + (regionHulls[a][b + 1].x - regionHulls[a][b].x) * c / 20,
+					y: regionHulls[a][b].y + (regionHulls[a][b + 1].y - regionHulls[a][b].y) * c / 20
+				}, gesturePts)) {
+					result.add(stateCodes[a])
+					break
+				}
+			}
+			if (result.has(stateCodes[a])) {
+				break
+			}
+		}
+	}
+	return [gesture[gesture.length - 1][2], Array.from(result)]
+}
--- public/javascripts/map_gesture.js
+++ public/javascripts/map_gesture.js
@@ -1,64 +1,64 @@
-let stateIDs = ["AK","AL","AZ","AR","CA","CO","CT","DE","FL","GA","HI","ID","IL","IN","IA","KS","KY","LA","ME","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC","ND","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY"]
+let stateIDs = ["AK", "AL", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
 let stateNames = {
-    "AL": "Alabama",
-    "AK": "Alaska",
-    "AS": "American Samoa",
-    "AZ": "Arizona",
-    "AR": "Arkansas",
-    "CA": "California",
-    "CO": "Colorado",
-    "CT": "Connecticut",
-    "DE": "Delaware",
-    "DC": "District Of Columbia",
-    "FM": "Federated States Of Micronesia",
-    "FL": "Florida",
-    "GA": "Georgia",
-    "GU": "Guam",
-    "HI": "Hawaii",
-    "ID": "Idaho",
-    "IL": "Illinois",
-    "IN": "Indiana",
-    "IA": "Iowa",
-    "KS": "Kansas",
-    "KY": "Kentucky",
-    "LA": "Louisiana",
-    "ME": "Maine",
-    "MH": "Marshall Islands",
-    "MD": "Maryland",
-    "MA": "Massachusetts",
-    "MI": "Michigan",
-    "MN": "Minnesota",
-    "MS": "Mississippi",
-    "MO": "Missouri",
-    "MT": "Montana",
-    "NE": "Nebraska",
-    "NV": "Nevada",
-    "NH": "New Hampshire",
-    "NJ": "New Jersey",
-    "NM": "New Mexico",
-    "NY": "New York",
-    "NC": "North Carolina",
-    "ND": "North Dakota",
-    "MP": "Northern Mariana Islands",
-    "OH": "Ohio",
-    "OK": "Oklahoma",
-    "OR": "Oregon",
-    "PW": "Palau",
-    "PA": "Pennsylvania",
-    "PR": "Puerto Rico",
-    "RI": "Rhode Island",
-    "SC": "South Carolina",
-    "SD": "South Dakota",
-    "TN": "Tennessee",
-    "TX": "Texas",
-    "UT": "Utah",
-    "VT": "Vermont",
-    "VI": "Virgin Islands",
-    "VA": "Virginia",
-    "WA": "Washington",
-    "WV": "West Virginia",
-    "WI": "Wisconsin",
-    "WY": "Wyoming"
+  "AL": "Alabama",
+  "AK": "Alaska",
+  "AS": "American Samoa",
+  "AZ": "Arizona",
+  "AR": "Arkansas",
+  "CA": "California",
+  "CO": "Colorado",
+  "CT": "Connecticut",
+  "DE": "Delaware",
+  "DC": "District Of Columbia",
+  "FM": "Federated States Of Micronesia",
+  "FL": "Florida",
+  "GA": "Georgia",
+  "GU": "Guam",
+  "HI": "Hawaii",
+  "ID": "Idaho",
+  "IL": "Illinois",
+  "IN": "Indiana",
+  "IA": "Iowa",
+  "KS": "Kansas",
+  "KY": "Kentucky",
+  "LA": "Louisiana",
+  "ME": "Maine",
+  "MH": "Marshall Islands",
+  "MD": "Maryland",
+  "MA": "Massachusetts",
+  "MI": "Michigan",
+  "MN": "Minnesota",
+  "MS": "Mississippi",
+  "MO": "Missouri",
+  "MT": "Montana",
+  "NE": "Nebraska",
+  "NV": "Nevada",
+  "NH": "New Hampshire",
+  "NJ": "New Jersey",
+  "NM": "New Mexico",
+  "NY": "New York",
+  "NC": "North Carolina",
+  "ND": "North Dakota",
+  "MP": "Northern Mariana Islands",
+  "OH": "Ohio",
+  "OK": "Oklahoma",
+  "OR": "Oregon",
+  "PW": "Palau",
+  "PA": "Pennsylvania",
+  "PR": "Puerto Rico",
+  "RI": "Rhode Island",
+  "SC": "South Carolina",
+  "SD": "South Dakota",
+  "TN": "Tennessee",
+  "TX": "Texas",
+  "UT": "Utah",
+  "VT": "Vermont",
+  "VI": "Virgin Islands",
+  "VA": "Virginia",
+  "WA": "Washington",
+  "WV": "West Virginia",
+  "WI": "Wisconsin",
+  "WY": "Wyoming"
 }
 
 let toSelectColor = '#888'
@@ -86,7 +86,6 @@ let dragCoords = [] // list of [x (normalized), y (normalized), time elapsed (ms
 
 let body = document.body, html = document.documentElement
 let margin = 0
-let ptThresh = 0.0005
 
 let data = []
 document.onkeyup = function (event) {
@@ -103,7 +102,7 @@ document.onkeyup = function (event) {
   }
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
   colorThese(toSelect, toSelectColor, defaultColor)
   initDimens()
   initPane()
@@ -135,7 +134,7 @@ function colorThese(states, color, otherColor) {
   for (let id of stateIDs) {
     stateColors[id] = statesSet.has(id) ? color : otherColor
   }
-  $('#map svg').find('path').each(function(){
+  $('#map svg').find('path').each(function () {
     $(this).css({ fill: stateColors[$(this).attr('class')] })
   })
 }
@@ -149,7 +148,7 @@ function onMouseDown(event) {
   dragCoords = []
   document.addEventListener('mousemove', onMouseMove)
   document.addEventListener('mouseup', onMouseUp)
-  selectTimer = setInterval(function() {
+  selectTimer = setInterval(function () {
     let elapsed = (Date.now() - dragStart) / 1000
     $('#timer').text(`${elapsed.toFixed(1)}s`)
   }, 100)
@@ -203,94 +202,4 @@ function hideDragCoords() {
     let ctx = canvas.getContext('2d')
     ctx.clearRect(0, 0, canvas.width, canvas.height)
   }
-}
-
-//gesture analysis functions return a list [a, b] where a is time taken for query in ms and b is the set of states selected
-function stabbingGesture(gesture) {
-  let result = new Set([])
-  for (let a = 0; a < 50; a++) {
-    for (let b = 0; b < regionPts[a].length; b++) {
-      for (let c = 0; c < gesture.length - 1; c++) {
-        //test if regionPts[a][b] is close enough to line segment gesture[c], gesture[c + 1]
-        let dist = distToSegment(regionPts[a][b], gesture[c], gesture[c + 1])
-        if (dist < ptThresh) {
-          result.add(stateCodes[a])
-        }
-      }
-    }
-  }
-  return [gesture[gesture.length - 1][2], Array.from(result)]
-}
-
-function wrappingInclusiveGesture(gesture) {
-  let result = new Set([])
-  let gesturePts = []
-
-  //compute gesturePts with .x and .y of gesture points to pass to convex hull function
-  for (let a = 0; a < gesture.length; a++) {
-    gesturePts.push({ x: gesture[a][0], y: gesture[a][1] })
-  }
-
-  for (let a = 0; a < 50; a++) {
-    for (let b = 0; b < regionPts[a].length; b++) {
-      if (ptInPoly({ x: regionPts[a][b][0], y: regionPts[a][b][1] }, gesturePts)) {
-        result.add(stateCodes[a])
-        break
-      }
-    }
-  }
-  return [gesture[gesture.length - 1][2], Array.from(result)]
-}
-
-function wrappingExclusiveGesture(gesture) {
-  let result = new Set([])
-  let gesturePts = []
-
-  //compute gesturePts with .x and .y of gesture points to pass to convex hull function
-  for (let a = 0; a < gesture.length; a++) {
-    gesturePts.push({ x: gesture[a][0], y: gesture[a][1] })
-  }
-
-  for (let a = 0; a < 50; a++) {
-    let shouldAdd = true
-    for (let b = 0; b < regionPts[a].length; b++) {
-      if (!ptInPoly({ x: regionPts[a][b][0], y: regionPts[a][b][1] }, gesturePts)) {
-        shouldAdd = false
-        break
-      }
-    }
-    if (shouldAdd) {
-      result.add(stateCodes[a])
-    }
-  }
-  return [gesture[gesture.length - 1][2], Array.from(result)]
-}
-
-function hullInclusiveGesture(gesture) {
-  let result = new Set([])
-  let gesturePts = []
-
-  //compute gesturePts with .x and .y of gesture points to pass to convex hull function
-  for (let a = 0; a < gesture.length; a++) {
-    gesturePts.push({ x: gesture[a][0], y: gesture[a][1] })
-  }
-
-  for (let a = 0; a < 50; a++) {
-    //add state if state convex hull @regionHulls[a] intersects gesture polygon at gesturePts
-    for (let b = 0; b < gesturePts.length - 1; b++) {
-      for (let c = 0; c <= 20; c++) {
-        if (ptInPoly({
-          x: gesturePts[b].x + (gesturePts[b + 1].x - gesturePts[b].x) * c / 20,
-          y: gesturePts[b].y + (gesturePts[b + 1].y - gesturePts[b].y) * c / 20
-        }, regionHulls[a])) {
-          result.add(stateCodes[a])
-          break
-        }
-      }
-      if (result.has(stateCodes[a])) {
-        break
-      }
-    }
-  }
-  return [gesture[gesture.length - 1][2], Array.from(result)]
 }
